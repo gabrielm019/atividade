f01ee6ab09d3ef97cb148fc8e56393c97b82d979
Gabriel Augusto <110576161+gabrielm019@users.noreply.github.com>
2023-03-15 22:25:16 -0300

    .

---
+++ atividade/ex02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <?php
+  for($impar=1; $impar<=100; $impar=$impar+2)
+{
+  print"<br>";
+  print($impar);
+  print"<br>";
+}
+  ?>
+</body>
+</html>
